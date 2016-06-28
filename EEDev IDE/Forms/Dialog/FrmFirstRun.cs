7047372cea90ffc01ee964c6af408749b0b20ca5
7ASecond-Net <admin@7asecond.net>
2016-06-28 21:47:00 +0100

    Added First Run Wizard Form

---
+++ EEDev IDE/Forms/Dialog/FrmFirstRun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EEDev_IDE.Forms.Dialog
+{
+    public partial class FrmFirstRun : Form
+    {
+        public FrmFirstRun()
+        {
+            InitializeComponent();
+        }
+    }
+}
