f95112e8fd1f377fa96690ad76a3bb352ec5c3b4
7ASecond-Net <admin@7asecond.net>
2016-06-29 19:51:34 +0100

    Added UserModel

---
+++ EEDev IDE/Data/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EEDev_IDE.Data.Models
+{
+    [Serializable]
+    internal class UserModel
+    {
+        [Key]
+        public string Email { get; set; }
+        public string Name { get; set; }        
+        public string Password { get; set; }
+
+        public bool GitHub { get; set; }
+        public bool Twitter { get; set; }
+        public bool Facebook { get; set; }
+
+    }
+}
